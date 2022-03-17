92b83ac700fb74cafa233d3e3606b2bdf1670cdb
Farjana Bintay Kamal <farjana.bintay.kamal@gmail.com>
2022-03-17 16:44:07 -0400

    Changes the queries

---
+++ Project_CuisineDelight/SQL Queries.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [dbo].[UserDetails] (
+    [Id]        INT              IDENTITY (1, 1) NOT NULL,
+    [FirstName] VARCHAR (50)     NOT NULL,
+    [LastName]  NVARCHAR (50)    NOT NULL,
+    [UserId]    UNIQUEIDENTIFIER NOT NULL,
+    [UserType]  NVARCHAR (50)    NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+CREATE PROCEDURE [dbo].[NEW_USER]
+		@userId  uniqueidentifier,
+		@firstname varchar(50),
+		@Lastname varchar(50),
+		@UserType varchar(50)
+AS
+	insert into UserDetails(FirstName, UserId,LastName,UserType) VALUES(@firstname,@userId,@Lastname,@UserType )
+RETURN
--- Project_CuisineDelight/SQLQuery2.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[UserDetials] (
-    [Id]        INT              IDENTITY (1, 1) NOT NULL,
-    [FirstName] VARCHAR (50)     NOT NULL,
-    [UserId]    UNIQUEIDENTIFIER NOT NULL,
-    [LastName] NVARCHAR(50) NOT NULL, 
-    [UserType] NVARCHAR(50) NOT NULL, 
-    PRIMARY KEY CLUSTERED ([Id] ASC)
-);
-
