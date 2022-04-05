2d2052371e4d4f254f8524cdc517f859ae4da449
Farjana Kamal <fkamal@confederationcollege.ca>
2022-04-05 17:10:59 -0400

    Added the stored procedure

--- Project_CuisineDelight/SQL Stored Prcedure.sql
+++ Project_CuisineDelight/SQL Stored Prcedure.sql
@@ -8,7 +8,6 @@ AS
 	insert into Packages(UserId,Package_Name,Package_Desc,Package_Discount,Package_Image) VALUES(@userId,@Package_Name,@Package_Desc,@Package_Discount,@Package_Image  )
 
 RETURN
-
 ---------------------------------------------
 
 CREATE PROCEDURE [dbo].[AddItem]
@@ -20,7 +19,12 @@ CREATE PROCEDURE [dbo].[AddItem]
 AS
 	insert into Items(UserId,Item_Name,Item_Desc, Item_Price, Item_Image) VALUES(@userId,@Item_Name  ,@Item_Desc,@Item_Price,@Item_Image)
 RETURN
-
+-----------------------------------
+CREATE PROCEDURE [dbo].[DeleteItem]
+ @Item_ID int
+AS
+	DELETE FROM Items WHERE Item_ID=@Item_ID;
+RETURN 0
 ------------------------------------------------
 CREATE PROCEDURE [dbo].[EditItem]
 		@Item_ID  int,
@@ -51,4 +55,14 @@ CREATE PROCEDURE [dbo].[NEW_USER]
       @Mobileno varchar(50)
 AS
    insert into UserDetails(FirstName,UserId,LastName,UserType,MobileNumber) VALUES(@firstname,@userId,@Lastname,@UserType,@Mobileno )
+RETURN
+-------------------------------------------------
+CREATE PROCEDURE [dbo].[NEW_USER_AS_Caterer]
+      @userId uniqueidentifier,
+      @description  NVARCHAR (150),
+      @UserType varchar(50),
+      @Mobileno varchar(50)
+AS
+   insert into UserDetails(UserId,UserType,MobileNumber) VALUES(@userId,@UserType,@Mobileno )
+   insert into CatererDetails(UserId,Description) values (@userId, @description)
 RETURN
